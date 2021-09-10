40e947a7a2dc7041785bfe29fc5ee7e6981e0f67
Mia van <82824582+MiavanRooijen@users.noreply.github.com>
2021-09-10 08:44:38 +0000

    goed stuiterende bal

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 var x, y, v;
 
 function setup() {
-	createCanvas(500, 400);
+	createCanvas(500, 400)
 
   x = 0;
   y = 200;
-  v = 5;
+  vx = 5;
+  vy = 5;
+  vn = 5;
+  vm = 5;
 }
 
 function draw() {
 	background(225);
-  
-  ellipse(x,y,50,50);
-  x= x + v;
+  let a = color('pink')
+  ellipse(x, y, 50, 50);
+  fill(a)
+  x = x + vx
+  y = y + vy
+ 
 
-  if(x <=0 || x > 500){
-    v= v*-1;
+  if(x < 0 || x > 500) {
+   vx = vx * -1;
+  }
+
+  if(y < 0 || y > 400) {
+   vy = vy * -1;
   }
 
 
